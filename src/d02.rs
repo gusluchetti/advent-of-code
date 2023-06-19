7df748a3a1a65c71b06a9de11e7f44163dfe3cb0
Gustavo Luchetti <gustavo@luchetti.dev>
2023-06-19 18:03:36 -0300

    feat: working on d02 solution

--- src/d02.rs
+++ src/d02.rs
@@ -8,14 +8,39 @@ struct Size {
     height: u8
 }
 
+impl Size {
+    fn total_area(&self) -> u32 {
+        (self.length * self.height * self.height) as u32
+    }
+}
+
 fn main() {
     task1();
     task2();
 }
 
 fn task1() -> () {
-    let input = parse_input(INPUT_PATH, Method::Other('x'));
+    let mut total_paper = 0;
+    let input = parse_input(INPUT_PATH, Method::NewLine);
     let input: Vec<String> = input.ok().unwrap();
+    println!("{:?}", input);
+
+    for n in input {
+        let split: Vec<&str> = n.split_terminator('x').collect();
+        if split.len() == 3 {
+            let length: u8 = split[0].parse().unwrap();
+            let width: u8 = split[1].parse().unwrap();
+            let height: u8 = split[2].parse().unwrap();
+
+            let box_size = Size {
+                length,
+                width,
+                height
+            };
+
+            let total_paper = total_paper + box_size.total_area();
+        }
+    }
 }
 
 fn task2() -> () {
