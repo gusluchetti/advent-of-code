ce2e89ca2f3d88c649d04582252f185aee32008a
Gustavo Luchetti <gustavo@luchetti.dev>
2023-06-20 11:34:13 -0300

    feat: solve d02 part 1 and 2

--- src/d02.rs
+++ src/d02.rs
@@ -2,15 +2,36 @@ use advent_of_code_2015::utils::{Method, parse_input};
 
 const INPUT_PATH: &str = "src/inputs/d02.txt";
 
+#[derive(Debug)]
 struct Size {
-    length: u8,
-    width: u8,
-    height: u8
+    length: u32,
+    width: u32,
+    height: u32
 }
 
 impl Size {
-    fn total_area(&self) -> u32 {
-        (self.length * self.height * self.height) as u32
+    fn surface_area(&self) -> u32 {
+        let two_lw = 2 * self.length * self.width;
+        let two_wh = 2 * self.width * self.height;
+        let two_hl = 2 * self.height * self.length;
+
+        two_lw + two_wh + two_hl
+    }
+
+    fn smallest_side(&self) -> u32 {
+        let mut vec = vec![self.length, self.width, self.height];
+        vec.sort();
+        vec[0] * vec[1]
+    }
+    
+    fn ribbon_size(&self) -> u32 {
+        let mut vec = vec![self.length, self.width, self.height];
+        vec.sort();
+        2*vec[0] + 2*vec[1]
+    }
+
+    fn cubic_volume(&self) -> u32 {
+        self.length * self.width * self.height
     }
 }
 
@@ -20,29 +41,54 @@ fn main() {
 }
 
 fn task1() -> () {
-    let mut total_paper = 0;
     let input = parse_input(INPUT_PATH, Method::NewLine);
     let input: Vec<String> = input.ok().unwrap();
-    println!("{:?}", input);
 
+    let mut total_paper = 0;
     for n in input {
         let split: Vec<&str> = n.split_terminator('x').collect();
         if split.len() == 3 {
-            let length: u8 = split[0].parse().unwrap();
-            let width: u8 = split[1].parse().unwrap();
-            let height: u8 = split[2].parse().unwrap();
+            let length: u32 = split[0].parse().unwrap();
+            let width: u32 = split[1].parse().unwrap();
+            let height: u32 = split[2].parse().unwrap();
 
             let box_size = Size {
                 length,
                 width,
                 height
             };
-
-            let total_paper = total_paper + box_size.total_area();
+            println!("{:?}", box_size);
+            let box_paper = box_size.surface_area() + box_size.smallest_side();
+            total_paper = total_paper + box_paper;
+            println!("total paper is: {}", total_paper);
         }
     }
+
+    println!("TOTAL paper needed: {}", total_paper);
 }
 
 fn task2() -> () {
+    let input = parse_input(INPUT_PATH, Method::NewLine);
+    let input: Vec<String> = input.ok().unwrap();
 
+    let mut total_ribbon = 0;
+    for n in input {
+        let split: Vec<&str> = n.split_terminator('x').collect();
+        if split.len() == 3 {
+            let length: u32 = split[0].parse().unwrap();
+            let width: u32 = split[1].parse().unwrap();
+            let height: u32 = split[2].parse().unwrap();
+
+            let box_size = Size {
+                length,
+                width,
+                height
+            };
+            println!("{:?}", box_size);
+            let ribbon = box_size.ribbon_size() + box_size.cubic_volume();
+            total_ribbon = total_ribbon + ribbon;
+        }
+    }
+
+    println!("TOTAL ribbon length needed: {}", total_ribbon);
 }
