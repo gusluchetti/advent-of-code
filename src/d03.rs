fb13dd89a79859daed70e5f02cc83055c5036872
Gustavo Luchetti <gustavo@luchetti.dev>
2023-06-20 11:39:21 -0300

    feat: prep day 3

---
+++ src/d03.rs
@@ -0,0 +1,16 @@
+use advent_of_code_2015::utils::{Method, parse_input};
+
+const INPUT_PATH: &str = "src/inputs/d03.txt";
+
+fn main() {
+    task1();
+    task2();
+}
+
+fn task1() {
+
+}
+
+fn task2() {
+
+}
