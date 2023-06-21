6a0c44f13ccde4464fa3be0700284e603001bab4
Gustavo Luchetti <gustavo@luchetti.dev>
2023-06-21 12:58:43 -0300

    feat: prep for day 4

---
+++ src/d04.rs
@@ -0,0 +1,19 @@
+use advent_of_code_2015::utils::{Method, parse_input};
+use std::collections::HashMap;
+
+const INPUT_PATH: &str = "src/inputs/d04.txt";
+
+fn main() {
+    task1();
+    task2();
+}
+
+// state: cartesian coordinate system
+// whats the best way to save it?
+fn task1() {
+
+}
+
+fn task2() {
+
+}
