046e9d1193c5ef697fe501515d8cd3050136a051
Gustavo Luchetti <gustavo@luchetti.dev>
2023-06-21 16:32:54 -0300

    feat: prep for day 5

---
+++ src/d05.rs
@@ -0,0 +1,17 @@
+use advent_of_code_2015::utils::{Method, parse_input, self};
+
+const INPUT_PATH: &str = "src/inputs/d05.txt";
+
+fn main() {
+    task1();
+    task2();
+}
+
+fn task1() -> () {
+    let input = utils::parse_input(INPUT_PATH, Method::NewLine);
+    println!("{:?}", input);
+}
+
+fn task2() -> () {
+
+}
