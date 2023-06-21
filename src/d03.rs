0562bc0cf698ae71e457bad1163fdf4186cc49ae
Gustavo Luchetti <gustavo@luchetti.dev>
2023-06-21 12:56:21 -0300

    feat: solve d03 task 2

--- src/d03.rs
+++ src/d03.rs
@@ -43,32 +43,39 @@ fn task2() {
         ("0,0".to_string(), 2)
     ]);
 
+    // santas coords
     let mut x1 = 0;
     let mut y1 = 0;
-
+    // robot santas coords
     let mut x2 = 0;
     let mut y2 = 0;
 
     let mut turn = 0;
 
-    fn get_new_coords(x: i32, y: i32) {
+    fn get_new_coords(dir: char, x: &mut i32, y: &mut i32) -> String {
         match dir {
-            '<' => {x = x-1;},
-            '>' => {x = x+1;},
-            '^' => {y = y+1;},
-            'v' => {y = y-1;},
+            '<' => *x -= 1,
+            '>' => *x += 1,
+            '^' => *y += 1,
+            'v' => *y -= 1,
             _ => {}
         }
+        let coords: String = format!["{x},{y}"];
+        coords
     }
     
+    let mut coords = String::from("0,0");
     for dir in directions.chars() {
-        if turn % 2 == 0 {
-            get_new_coords(x1, y1)
+        if turn == 0 || turn % 2 == 0 {
+            coords = get_new_coords(dir, &mut x1, &mut y1);
+            println!("santa cur. coords after {dir}: {:?}", coords);
+        } else { 
+            coords = get_new_coords(dir, &mut x2, &mut y2); 
+            println!("robot santa cur. coords after {dir}: {:?}", coords);
         }
-        let coords: String = format!["{x},{y}"];
-        println!("{:?}", coords);
-        known.entry(coords)
-            .and_modify(|counter| { *counter += 1 })
-            .or_insert(1); 
+        known.entry(coords).and_modify(|counter| { *counter += 1 }).or_insert(1); 
+        turn = turn + 1;
     }
+    println!("{:?}", known);
+    println!("houses that got >= 1 gift: {:?}", known.len());
 }
