b30a19a62fe0f5a62a95f9227fc11c9730d94764
Gustavo Luchetti <gustavo@luchetti.dev>
2023-06-21 14:02:07 -0300

    feat: wip d04 task 1

--- src/d04.rs
+++ src/d04.rs
@@ -1,19 +1,41 @@
 use advent_of_code_2015::utils::{Method, parse_input};
-use std::collections::HashMap;
 
 const INPUT_PATH: &str = "src/inputs/d04.txt";
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn first_md5_hash() {
+        let secret = "abcdef";
+        let answer = 609043;
+        let result = 0;
+
+        assert_eq![result, answer];
+    }
+
+    #[test]
+    fn second_md5_hash() {
+        let secret = "pqrstuv";
+        let answer = 1048970;
+        let result = 0;
+
+        assert_eq![result, answer];
+    }
+}
 
 fn main() {
     task1();
     task2();
 }
 
-// state: cartesian coordinate system
-// whats the best way to save it?
 fn task1() {
-
+    let input = parse_input(INPUT_PATH, Method::OneString);
+    let secret: &str = input[0].trim();
+    println!("{:?}", secret);
 }
 
 fn task2() {
-
+    let input = parse_input(INPUT_PATH, Method::OneString);
+    let secret: &str = input[0].trim();
+    println!("{:?}", secret);
 }
