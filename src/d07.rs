6c41cd11210978c8a97fa56753c634f3f25b8590
Gustavo Luís Luchetti <gustavo@luchetti.dev>
2023-06-24 14:47:34 -0300

    feat: wip day 07 start

--- src/d07.rs
+++ src/d07.rs
@@ -1,16 +1,30 @@
-use advent_of_code_2015::utils::{parse_input, Method};
-
 const INPUT_PATH: &str = "src/inputs/d07.txt";
 
+#[cfg(test)]
+mod tests {
+    use super::*;
+    const INPUT_TEST: &str = "src/inputs/test_d07.txt";
+
+    #[test]
+    fn test_sample_circuit() {
+        
+    }
+}
+
+
 fn main() {
-    task1();
+    let lines = std::fs::read_to_string(INPUT_PATH)
+        .expect("cargo should be run from root")
+        .lines()
+        .map(String::from)
+        .collect();
+
+    task1(lines);
     task2();
 }
 
-fn task1() -> () {
-
+fn task1(lines: Vec<String>) -> () {
+    println!("{:?}", lines);
 }
 
-fn task2() -> () {
-
-}
+fn task2() -> () {}
