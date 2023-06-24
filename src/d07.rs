06ad68dd6a2cffd460f206893db64bf30f684a0a
Gustavo Luís Luchetti <gustavo@luchetti.dev>
2023-06-24 16:17:52 -0300

    wip: first solution day 07
    idea: run once, set all circuits, THEN try and parse each of the variables so we are never left without a u16 value

--- src/d07.rs
+++ src/d07.rs
@@ -1,19 +1,36 @@
+use std::collections::HashMap;
+
 const INPUT_PATH: &str = "src/inputs/d07.txt";
 
 #[cfg(test)]
 mod tests {
     use super::*;
+
     const INPUT_TEST: &str = "src/inputs/test_d07.txt";
 
     #[test]
     fn test_sample_circuit() {
-        
+        let lines: Vec<String> = std::fs::read_to_string(INPUT_TEST)
+            .expect("cargo should be run from root")
+            .lines()
+            .map(String::from)
+            .collect();
+
+        let res = read_circuit(lines);
+
+        assert_eq!(res["d"], 72);
+        assert_eq!(res["e"], 507);
+        assert_eq!(res["f"], 492);
+        assert_eq!(res["g"], 114);
+        assert_eq!(res["h"], 65412);
+        assert_eq!(res["i"], 65079);
+        assert_eq!(res["x"], 123);
+        assert_eq!(res["y"], 456);
     }
 }
 
-
 fn main() {
-    let lines = std::fs::read_to_string(INPUT_PATH)
+    let lines: Vec<String> = std::fs::read_to_string(INPUT_PATH)
         .expect("cargo should be run from root")
         .lines()
         .map(String::from)
@@ -23,8 +40,63 @@ fn main() {
     task2();
 }
 
+fn get_value_or_current(circuit: &HashMap<String, u16>, s: &str) -> u16 {
+    let cur: u16;
+    let value = s.parse::<u16>();
+    println!("{:?}", circuit);
+    if value.is_err() {
+        cur = circuit[&String::from(s)];
+    } else {
+        cur = value.ok().unwrap();
+    }
+
+    return cur;
+}
+
+fn read_circuit(lines: Vec<String>) -> HashMap<String, u16> {
+    let mut circuit: HashMap<String, u16> = HashMap::new();
+
+    for line in lines {
+        let mut split: Vec<&str> = line.split_whitespace().collect();
+        let receiver = split.pop().expect("should always have receiver");
+        split.pop().unwrap(); // disconsider '->'
+        let sender = &split[..];
+
+        let mut msg: u16 = 0;
+        println!("{:?} -> {:?}", sender, receiver);
+        match sender.len() {
+            3 => {
+                let first = get_value_or_current(&circuit, sender[0]);
+                let second = get_value_or_current(&circuit, sender[2]);
+
+                msg = match sender[1] {
+                    "AND" => first & second,
+                    "OR" => first | second,
+                    "RSHIFT" => first >> second,
+                    "LSHIFT" => first << second,
+                    _ => 0 as u16,
+                }
+            }
+            2 => {
+                msg = !get_value_or_current(&circuit, sender[1]);
+            }
+            // just a value assignment
+            1 => {
+                msg = get_value_or_current(&circuit, sender[0]);
+            }
+            _ => {}
+        }
+
+        circuit.entry(receiver.to_string()).or_insert(msg);
+        println!("{:?}\n", circuit);
+    }
+
+    circuit
+}
+
 fn task1(lines: Vec<String>) -> () {
-    println!("{:?}", lines);
+    let res = read_circuit(lines);
+    println!("{:?}", res);
 }
 
 fn task2() -> () {}
