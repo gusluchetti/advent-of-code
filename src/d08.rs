dd0d2e4cb292a0e5c88eaa30eac9df1fafe4dfa7
Gustavo Luís Luchetti <gustavo@luchetti.dev>
2023-06-27 23:11:52 -0300

    chore: day 8 prep

---
+++ src/d08.rs
@@ -0,0 +1,16 @@
+use advent_of_code_2015::utils::{parse_input, Method};
+
+const INPUT_PATH: &str = "src/inputs/d08.txt";
+
+fn main() {
+    task1();
+    task2();
+}
+
+fn task1() -> () {
+
+}
+
+fn task2() -> () {
+
+}
