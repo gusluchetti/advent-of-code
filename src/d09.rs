99bd5efcd4898bf99a349f683c8a23084dd2c3e7
Gustavo Luís Luchetti <gustavo@luchetti.dev>
2023-06-28 14:39:56 -0300

    feat: start day 9 prep

---
+++ src/d09.rs
@@ -0,0 +1,15 @@
+const INPUT_PATH: &str = "src/inputs/d09.txt";
+
+fn main() {
+    let input = std::fs::read_to_string(INPUT_PATH).expect("should have input");
+    task1(&input);
+    task2(&input);
+}
+
+fn task1(input: &String) {
+
+}
+
+fn task2(input: &String) {
+
+}
