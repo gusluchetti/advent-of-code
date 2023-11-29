4817c5593ff83a0c95e4db49a003ea0e992b8567
Gustavo Luís Luchetti <gustavo@luchetti.dev>
2023-11-28 23:56:35 -0300

    feat: close to end day one

--- D01/main.lua
+++ D01/main.lua
@@ -1,3 +1,5 @@
+local pp = require("pl.pretty")
+
 io.input("./input.txt")
 local file = io.read("a");
 print(file)
@@ -10,9 +12,12 @@ local dir_index = 1;
 local plane = {};
 plane.x = 0
 plane.y = 0
+visited = {}
+table.insert(visited, { x = 0, y = 0 })
 
 for inst in string.gmatch(file, "[R,L]%d+") do
-    print("index: ", dir_index, inst)
+    print()
+    print(inst)
     if string.find(inst, "R") then
         if dir_index == 4 then
             dir_index = 1
@@ -28,17 +33,47 @@ for inst in string.gmatch(file, "[R,L]%d+") do
     end
 
     local number = string.match(inst, "%d+")
+
+    print('start', plane.x, plane.y)
     if dir_index == 1 then
+        for i = 1, number do
+            local item = { x = plane.x, y = plane.y + i }
+            if table[item] ~= nil then
+                print('intersection: ', item)
+            end
+            table.insert(visited, item)
+        end
         plane.y = plane.y + number
     elseif dir_index == 2 then
+        for i = 1, number do
+            local item = { x = plane.x + i, y = plane.y }
+            if table[item] ~= nil then
+                print('intersection: ', item)
+            end
+            table.insert(visited, item)
+        end
         plane.x = plane.x + number
     elseif dir_index == 3 then
+        for i = 1, number do
+            local item = { x = plane.x, y = plane.y - i }
+            if table[item] ~= nil then
+                print('intersection: ', item)
+            end
+            table.insert(visited, item)
+        end
         plane.y = plane.y - number
     elseif dir_index == 4 then
+        for i = 1, number do
+            local item = { x = plane.x - i, y = plane.y }
+            if table[item] ~= nil then
+                print('intersection: ', item)
+            end
+            table.insert(visited, item)
+        end
         plane.x = plane.x - number
     end
-
-    print(plane.x, plane.y)
+    print('end', plane.x, plane.y)
 end
 
+pp.dump(visited)
 print(math.abs(plane.x) + math.abs(plane.y))
