090e63cb4f1ef4ea092c21fb8ced6af26c0d7860
Gustavo Luchetti <gustavo@luchetti.dev>
2023-11-30 18:51:39 -0300

    feat: doing d10

--- src/d10.rs
+++ src/d10.rs
@@ -1,26 +1,28 @@
 const INPUT_PATH: &str = "src/inputs/d10.txt";
 
 fn main() {
-    let input = std::fs::read_to_string(INPUT_PATH).expect("should have input");
+    // let input = std::fs::read_to_string(INPUT_PATH).expect("should have input");
     // let mut line: &str = input.lines().collect::<Vec<&str>>()[0];
-    let mut line: &str = "1";
+    let mut line: String = String::from("1");
+    let mut new_line: String;
 
     let stop = 5;
-    let mut count = 0;
-    let mut prev: char = 'j';
-    let mut new_line: String = "".to_string();
+    for n in 0..stop {
+        new_line = "".to_string();
+        let length = line.len();
+        let mut count = 1;
+        let mut prev: char = 'x';
 
-    for _ in 0..stop {
-        println!("{:?}\n", line);
-        for (i, c) in line.to_owned().chars().enumerate() {
+        for (i, c) in line.chars().enumerate() {
             println!("cur: {c} w/ count ({count}), prev: {prev}");
-            if i == 0 {
+
+            if i == 0 && length == 1 {
                 prev = c;
             }
 
-            if c != prev {
-                println!("done w/ {prev}");
-                new_line += &count.to_string();
+            if c != prev || (i == length - 1) {
+                println!("adding to newline: {count}, {prev}");
+                new_line.push_str(&count.to_string());
                 new_line.push(prev);
 
                 count = 0;
@@ -29,6 +31,8 @@ fn main() {
 
             count += 1;
         }
-        line = &new_line;
+
+        line = new_line.clone();
+        println!("\npass {:x}: {:?}", n + 1, line);
     }
 }
