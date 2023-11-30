f31cbf1c26469978982ceac6cfbdecd61127f930
Gustavo Luís Luchetti <gustavo@luchetti.dev>
2023-11-30 20:31:25 -0300

    feat: solve day 10

--- src/d10.rs
+++ src/d10.rs
@@ -1,38 +1,42 @@
 const INPUT_PATH: &str = "src/inputs/d10.txt";
 
 fn main() {
-    // let input = std::fs::read_to_string(INPUT_PATH).expect("should have input");
-    // let mut line: &str = input.lines().collect::<Vec<&str>>()[0];
-    let mut line: String = String::from("1");
+    let _input = std::fs::read_to_string(INPUT_PATH).expect("should have input");
+    let mut line: String = _input.lines().collect::<Vec<&str>>()[0].to_string();
+    // let mut line = String::from("1");
     let mut new_line: String;
 
-    let stop = 5;
+    let stop = 50;
     for n in 0..stop {
-        new_line = "".to_string();
+        new_line = String::from("");
         let length = line.len();
         let mut count = 1;
         let mut prev: char = 'x';
 
         for (i, c) in line.chars().enumerate() {
-            println!("cur: {c} w/ count ({count}), prev: {prev}");
+            // println!("cur: {c} w/ count ({count}), prev: {prev}");
 
-            if i == 0 && length == 1 {
-                prev = c;
-            }
-
-            if c != prev || (i == length - 1) {
-                println!("adding to newline: {count}, {prev}");
+            if i != 0 && c != prev {
+                // println!("adding prev -> {count}{prev}");
                 new_line.push_str(&count.to_string());
                 new_line.push(prev);
-
-                count = 0;
-                prev = c;
+                count = 1;
             }
 
-            count += 1;
+            if c == prev {
+                count += 1;
+            }
+
+            if i == length - 1 {
+                // println!("adding cur -> {count}{c}");
+                new_line.push_str(&count.to_string());
+                new_line.push(c);
+            }
+
+            prev = c;
         }
 
         line = new_line.clone();
-        println!("\npass {:x}: {:?}", n + 1, line);
+        println!("pass {:?} length: {:?}", n + 1, line.len());
     }
 }
