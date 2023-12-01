b8004f8afffa12d2ea1acd02f85200000fae93ed
Gustavo Luchetti <gustavo@luchetti.dev>
2023-12-01 12:04:40 -0300

    feat: add inputs and solve p1 d01

--- d01.py
+++ d01.py
@@ -1,5 +1,28 @@
+import re
+
 def main():
-    print("hello world!");
+    file = open("./inputs/d01.txt", "r", encoding="utf-8")
+
+    search = r'\d'
+    include_written_nums = False
+    p = re.compile(search)
+    sum = 0
+
+    for line in file:
+        if (include_written_nums):
+            digits = [0,1]
+        else:
+            digits = p.findall(line)
+
+        print(line, digits)
+        first = digits[0]
+        last = digits[-1]
+        new_num = int("{}{}".format(first, last))
+        print(first, last, new_num)
+        sum += new_num
+
+    print(sum)
+
 
 if __name__ == "__main__":
     main()
