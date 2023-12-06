67e4272b14fb106645271db091799c7e653388bc
Gustavo Luchetti <gustavo@luchetti.dev>
2023-12-06 14:58:20 -0300

    feat: solve d6

--- src/d06.py
+++ src/d06.py
@@ -1,23 +1,72 @@
+from functools import reduce
 from pathlib import Path
 import time
+import math
 
-def part1(file):
-    for line in file:
-        print(line)
-    return 0
+def part1(lines):
+    times = [int(a) for a in lines[0].split()[1:]]
+    distances = [int(b) for b in lines[1].split()[1:]]
+    length = len(times)
 
-def part2(file):
-    for line in file:
-        print(line)
-    return 0
+    winning_ways: list[int] = []
+    for i in range(0, length):
+        win, has_won = 0, False
+        time, distance = times[i], distances[i]
+        print(time, distance)
+
+        for i in range(1, time):
+            remainder = time - i 
+            new_dist = i * remainder
+            if new_dist > distance:
+                win += 1
+                has_won = True
+            else:
+                if has_won: break
+
+        print(f'can win in {win} ways')
+        winning_ways.append(win)
+
+    print(winning_ways)
+    return math.prod(winning_ways)
+
+def part2(lines):
+    times = [int(''.join(lines[0].split()[1:]))]
+    distances = [int(''.join(lines[1].split()[1:]))]
+    print(times, distances)
+    length = len(times)
+
+    winning_ways: list[int] = []
+    for i in range(0, length):
+        win, has_won = 0, False
+        time, distance = times[i], distances[i]
+        print(time, distance)
+
+        for i in range(1, time):
+            remainder = time - i 
+            new_dist = i * remainder
+            if new_dist > distance:
+                win += 1
+                has_won = True
+            else:
+                if has_won: break
+
+        print(f'can win in {win} ways')
+        winning_ways.append(win)
+
+    print(winning_ways)
+    return math.prod(winning_ways)
 
 def main():
     input = Path(__file__).parent / f'../inputs/{Path(__file__).stem}.txt'
     # input = Path(__file__).parent / f'../inputs/test_{Path(__file__).stem}.txt'
-    file = open(input, "r", encoding="utf-8")
 
-    p1 = part1(file)
-    p2 = part2(file)
+    print()
+    file = open(input, "r", encoding="utf-8")
+    p1 = part1(file.readlines())
+
+    file = open(input, "r", encoding="utf-8")
+    p2 = part2(file.readlines())
+
     return (p1, p2)
 
 if __name__ == "__main__":
