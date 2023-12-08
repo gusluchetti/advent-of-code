1203c45482b37d938ea7858f3ed91775c967d0d4
Gustavo Luchetti <gustavo@luchetti.dev>
2023-12-08 11:35:02 -0300

    feat: solve d8 p1

--- src/d08.py
+++ src/d08.py
@@ -2,8 +2,38 @@ from pathlib import Path
 import time
 
 def part1(lines):
-    print(lines)
-    return -1
+    nodes = {}
+    directions, *info = lines
+    directions = directions.strip()
+    for i in info:
+        i = i.strip().split()
+        if len(i) >= 1:
+            cur = i[0]
+            left = i[2][1:4]
+            right = i[3][0:3]
+            nodes[cur] = {'cur': cur, 'left': left, 'right': right}
+
+    state = nodes['AAA']
+    steps = 0
+    done = False
+
+    print(f'\nstarting...')
+    print(nodes, len(nodes))
+    print(directions,  len(directions))
+    print(state, steps)
+
+    while not done: 
+        for d in directions:
+            steps += 1
+            match d:
+                case 'L': state = nodes[state['left']]
+                case 'R': state = nodes[state['right']]
+            if state['cur'] == 'ZZZ':
+                done = True
+                break
+
+    print(f'steps taken: {steps}')
+    return steps 
 
 def part2(lines):
     print(lines)
@@ -11,7 +41,7 @@ def part2(lines):
 
 def main():
     input = Path(__file__).parent / f'../inputs/{Path(__file__).stem}.txt'
-    input = Path(__file__).parent / f'../inputs/test_{Path(__file__).stem}.txt'
+    # input = Path(__file__).parent / f'../inputs/test_{Path(__file__).stem}.txt'
     file = open(input, "r", encoding="utf-8")
 
     p1 = part1(file.readlines())
