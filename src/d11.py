7a4913421e6f2542e56b1f297343ff4ba1fa999a
Gustavo Luchetti <gustavo@luchetti.dev>
2023-12-11 10:57:07 -0300

    feat: solve nice d11

--- src/d11.py
+++ src/d11.py
@@ -1,37 +1,19 @@
 from pathlib import Path
 import time
 
-def part1(lines: list[str]):
+def part1(lines: list[str], expansion: int = 1):
     lines = [l.strip() for l in lines]
-    print(f'\nbefore expansion: {lines}')
     row_length = len(lines[0])
 
     expand = []
     for i, row in enumerate(lines): 
         if not row.__contains__('#'):
             expand.append(('r', i))
-
     for col_idx in range(0, row_length): 
         column = [line[col_idx] for line in lines]
         joined = ''.join(column)
         if not joined.__contains__('#'):
             expand.append(('c', col_idx))
-
-    new_rows, new_cols = 0, 0
-    for e in expand:
-        t, index = e
-        if t == 'r':
-            print(f'new row', t, index)
-            lines.insert(index+new_rows, '.' * row_length)
-            new_rows += 1
-        elif t == 'c':
-            print(f'new col', t, index)
-            for i in range(0, len(lines)):
-                print(lines[i])
-                lines[i] = lines[i][0:index+new_cols] + '.' + lines[i][index+new_cols:]
-                print(lines[i])
-            new_cols += 1
-    print(f'\nafter expansion: {lines}')
 
     galaxies = []
     for y, row in enumerate(lines):
@@ -39,34 +21,76 @@ def part1(lines: list[str]):
             if char == '#':
                 galaxies.append((y, x))
 
-    print(f'\n{lines}')
-
     min_distances = 0
     for i, gc in enumerate(galaxies):
-        print(i+1, gc)
         others = galaxies[i+1:]
-        print(others)
         for o in others:
-            min_y = abs(gc[0] - o[0])  
-            min_x = abs(gc[1] - o[1])  
+            cy, cx = gc
+            oy, ox = o
+
+            y_range = range(min(cy, oy), max(cy, oy)+1)
+            relevant = [e for e in expand if e[0] == 'r' and e[1] in y_range]
+            min_y = abs(cy - oy) + (len(relevant)*expansion)  
+
+            x_range = range(min(cx, ox), max(cx, ox)+1)
+            relevant = [e for e in expand if e[0] == 'c' and e[1] in x_range]
+            min_x = abs(cx - ox) + (len(relevant)*expansion)  
+
             min_dist = min_y + min_x
             min_distances += min_dist
-            print(f'min dist to {o} from {gc} = {min_dist}')
 
     return min_distances
 
-def part2(lines):
-    print(lines)
-    return -1
+def part2(lines: list[str], expansion: int = 1):
+    lines = [l.strip() for l in lines]
+    row_length = len(lines[0])
+
+    expand = []
+    for i, row in enumerate(lines): 
+        if not row.__contains__('#'):
+            expand.append(('r', i))
+    for col_idx in range(0, row_length): 
+        column = [line[col_idx] for line in lines]
+        joined = ''.join(column)
+        if not joined.__contains__('#'):
+            expand.append(('c', col_idx))
+
+    galaxies = []
+    for y, row in enumerate(lines):
+        for x, char in enumerate(row):
+            if char == '#':
+                galaxies.append((y, x))
+
+    min_distances = 0
+    for i, gc in enumerate(galaxies):
+        others = galaxies[i+1:]
+        for o in others:
+            cy, cx = gc
+            oy, ox = o
+
+            y_range = range(min(cy, oy), max(cy, oy)+1)
+            relevant = [e for e in expand if e[0] == 'r' and e[1] in y_range]
+            min_y = abs(cy - oy) + (len(relevant)*expansion)  
+
+            x_range = range(min(cx, ox), max(cx, ox)+1)
+            relevant = [e for e in expand if e[0] == 'c' and e[1] in x_range]
+            min_x = abs(cx - ox) + (len(relevant)*expansion)  
+
+            min_dist = min_y + min_x
+            min_distances += min_dist
+
+    return min_distances
 
 def main():
     input = Path(__file__).parent / f'../inputs/{Path(__file__).stem}.txt'
-    input = Path(__file__).parent / f'../inputs/test_{Path(__file__).stem}.txt'
-    file = open(input, "r", encoding="utf-8")
+    # input = Path(__file__).parent / f'../inputs/test_{Path(__file__).stem}.txt'
 
-    p1 = part1(file.readlines())
-    # p2 = part2(file.readlines())
-    return (p1, -1)
+    file = open(input, "r", encoding="utf-8")
+    p1 = part1(file.readlines(), 1)
+
+    file = open(input, "r", encoding="utf-8")
+    p2 = part2(file.readlines(), 1_000_000 - 1)
+    return (p1, p2)
 
 if __name__ == "__main__":
     t = time.perf_counter_ns()
