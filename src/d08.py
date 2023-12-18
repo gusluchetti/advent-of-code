6aa3ed8989ff95c6106d6acf33b71f15ac0118d7
Gustavo Luís Luchetti <gustavo@luchetti.dev>
2023-12-18 01:17:06 -0300

    chore: testing nvim setup

--- src/d08.py
+++ src/d08.py
@@ -1,6 +1,6 @@
-from pathlib import Path
-import time
 import math
+import time
+from pathlib import Path
 
 test_input = """LR\n
 \n
@@ -13,6 +13,7 @@ test_input = """LR\n
 22Z = (22B, 22B)\n
 XXX = (XXX, XXX)\n""".splitlines()
 
+
 def part1(lines):
     nodes = {}
     directions, *info = lines
@@ -23,29 +24,32 @@ def part1(lines):
             cur = i[0]
             left = i[2][1:4]
             right = i[3][0:3]
-            nodes[cur] = {'cur': cur, 'left': left, 'right': right}
+            nodes[cur] = {"cur": cur, "left": left, "right": right}
     print(nodes, len(nodes))
 
-    state = nodes['AAA']
+    state = nodes["AAA"]
     steps = 0
     done = False
 
-    print(f'\nstarting...')
-    print(directions,  len(directions))
+    print("\nstarting...")
+    print(directions, len(directions))
     print(state, steps)
 
-    while not done: 
+    while not done:
         for d in directions:
             steps += 1
             match d:
-                case 'L': state = nodes[state['left']]
-                case 'R': state = nodes[state['right']]
-            if state['cur'] == 'ZZZ':
+                case "L":
+                    state = nodes[state["left"]]
+                case "R":
+                    state = nodes[state["right"]]
+            if state["cur"] == "ZZZ":
                 done = True
                 break
 
-    print(f'steps taken: {steps}')
-    return steps 
+    print(f"steps taken: {steps}")
+    return steps
+
 
 def part2(lines):
     nodes = {}
@@ -56,37 +60,41 @@ def part2(lines):
         if len(i) >= 1:
             cur = i[0]
             left, right = i[2][1:4], i[3][0:3]
-            nodes[cur] = {'cur': cur, 'left': left, 'right': right}
+            nodes[cur] = {"cur": cur, "left": left, "right": right}
 
-    states = [v for _, v in nodes.items() if v['cur'][2] == 'A']
+    states = [v for _, v in nodes.items() if v["cur"][2] == "A"]
     steps = 0
     done = False
-    print(f'\nstarting...')
-    print(directions,  len(directions))
+    print("\nstarting...")
+    print(directions, len(directions))
     print(states, len(states), steps)
 
     finished_mins = [0] * len(states)
-    while not done: 
+    while not done:
         for d in directions:
             steps += 1
             for i, s in enumerate(states):
                 match d:
-                    case 'L': states[i] = nodes[s['left']]
-                    case 'R': states[i] = nodes[s['right']]
-            finished = [state['cur'][2] == 'Z' for state in states]
+                    case "L":
+                        states[i] = nodes[s["left"]]
+                    case "R":
+                        states[i] = nodes[s["right"]]
+            finished = [state["cur"][2] == "Z" for state in states]
 
             for i, f in enumerate(finished):
-                if f: finished_mins[i] = steps
+                if f:
+                    finished_mins[i] = steps
 
             if all([f != 0 for f in finished_mins]):
                 done = True
                 break
 
     print(finished_mins)
-    return math.lcm(*finished_mins) 
+    return math.lcm(*finished_mins)
+
 
 def main():
-    input = Path(__file__).parent / f'../inputs/{Path(__file__).stem}.txt'
+    input = Path(__file__).parent / f"../inputs/{Path(__file__).stem}.txt"
     # input = Path(__file__).parent / f'../inputs/test_{Path(__file__).stem}.txt'
 
     file = open(input, "r", encoding="utf-8")
@@ -97,9 +105,10 @@ def main():
 
     return (p1, p2)
 
+
 if __name__ == "__main__":
     t = time.perf_counter_ns()
     (p1, p2) = main()
     elapsed = time.perf_counter_ns() - t
     print(p1, p2)
-    print('elapsed: {}s'.format(elapsed/1000000000))
+    print("elapsed: {}s".format(elapsed / 1000000000))
