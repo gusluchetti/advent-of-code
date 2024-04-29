570395f18a3c6c1ebc2e59b8769a5dfb657f14ab
gusluchetti <gustavo@luchetti.dev>
2024-04-29 12:10:16 -0300

    feat: changing 2016 to cpp

---
+++ 2016/D01/main.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+#include <string>
+#include <istream>
+
+using namespace std;
+
+int main() {
+  string dir;
+  while(getline(cin, dir, ',')) {
+    cout << dir << "\n";
+  }
+
+  return 0;
+}
