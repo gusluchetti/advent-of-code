da4da5cfed6e8bc4703eda7c20be08f7c9e23a27
Gustavo Luchetti <gustavo@luchetti.dev>
2024-05-03 23:17:07 -0300

    feat: swapping lua for c++
    i am struggling with input parsing hehe, but still, better than random
    languages

--- 2016/D01/main.cpp
+++ 2016/D01/main.cpp
@@ -1,13 +1,36 @@
 #include <bits/stdc++.h>
 #include <string>
-#include <istream>
 
 using namespace std;
 
 int main() {
-  string dir;
-  while(getline(cin, dir, ',')) {
-    cout << dir << "\n";
+  int x = 0;
+  int y = 0;
+  int dir; // 0 up, 1 right, 2 down, 3 left
+  string input;
+
+  while (getline(cin, input, ' ')) {
+    cout << input << "\n";
+    for (int i = 0; i < 3; i++) {
+      if (i == 0) {
+        char turn_dir = input[i];
+        if (turn_dir == 'L') {
+        }
+        if (turn_dir == 'R') {
+        }
+      } else if (i == 1) {
+        char blocks = input[i];
+        if (dir == 0) {
+          y++;
+        } else if (dir == 1) {
+          x++;
+        } else if (dir == 2) {
+          y--;
+        } else if (dir == 3) {
+          x--;
+        }
+      }
+    }
   }
 
   return 0;
