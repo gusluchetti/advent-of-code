764ca081b0ab3241f2a3ed30f602c0a13524c24a
Gustavo Luchetti <gustavo@luchetti.dev>
2024-12-01 10:50:56 -0300

    feat: solve part 1

--- 2024/D01/main.cpp
+++ 2024/D01/main.cpp
@@ -1,4 +1,9 @@
+#include <algorithm>
+#include <cstdlib>
+#include <ios>
 #include <iostream>
+#include <sstream>
+#include <vector>
 
 int main() {
   std::ios::sync_with_stdio(0);
@@ -7,8 +12,28 @@ int main() {
   freopen("output.txt", "w", stdout);
 
   std::string line;
+  std::vector<int> left_list;
+  std::vector<int> right_list;
   while (std::cin) {
     getline(std::cin, line);
-    std::cout << line << "\n";
+    std::stringstream ss(line);
+
+    int number;
+    while (ss >> number) {
+      left_list.push_back(number);
+      ss >> number;
+      right_list.push_back(number);
+    }
   }
+
+  std::sort(left_list.begin(), left_list.end());
+  std::sort(right_list.begin(), right_list.end());
+
+  int sum = 0;
+  int size = left_list.size();
+  for (int i = 0; i < size; i++) {
+    sum += abs(left_list[i] - right_list[i]);
+  }
+
+  std::cout << sum;
 }
