fa900ef11efcd5281a7b95baababf27f762b5e98
Gustavo Luchetti <gustavo@luchetti.dev>
2024-12-02 10:40:53 -0300

    feat: reorganize d01

--- 2024/D01/main.cpp
+++ 2024/D01/main.cpp
@@ -8,8 +8,6 @@
 int main() {
   std::ios::sync_with_stdio(0);
   std::cin.tie(0);
-  freopen("input.txt", "r", stdin);
-  freopen("output.txt", "w", stdout);
 
   std::string line;
   std::vector<int> left_list;
