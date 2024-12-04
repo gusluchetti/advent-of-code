11fc7129333f490d6ce5c402ec68be3f2ea38106
Gustavo Luchetti <gustavo@luchetti.dev>
2024-12-04 14:14:30 -0300

    wip: check for x-mas

--- 2024/D04/main.cpp
+++ 2024/D04/main.cpp
@@ -3,11 +3,51 @@
 #include <string>
 #include <vector>
 
+int count_x_mas_occurrences(std::vector<std::vector<char>> &ws, int x, int y) {
+  int count = 0;
+  int max_y = ws.size();
+  int max_x = ws[0].size();
+  // check 4 possible directions, assuming ltr checks if possible
+  // 1. Ms on TOP, first M on left
+  if (y <= max_y - 4 && x <= max_x - 4) {
+    std::string mas_1 = {ws[y][x], ws[y + 1][x + 1], ws[y + 2][x + 2]};
+    std::string mas_2 = {ws[y][x + 2], ws[y + 1][x + 1], ws[y + 2][x]};
+    if (mas_1 == "MAS" && mas_2 == "MAS") {
+      count++;
+    }
+  }
+  // 2. Ms on RIGHT, first M on top
+  if (y <= max_y - 4 && x >= 3) {
+    std::string mas_1 = {ws[y][x], ws[y + 1][x - 1], ws[y + 2][x - 2]};
+    std::string mas_2 = {ws[y + 2][x], ws[y + 1][x - 1], ws[y][x - 2]};
+    if (mas_1 == "MAS" && mas_2 == "MAS") {
+      count++;
+    }
+  }
+  // 3. Ms on DOWN, first M on left
+  if (y >= 3 && x <= max_x - 4) {
+    std::string mas_1 = {ws[y][x], ws[y - 1][x + 1], ws[y - 2][x + 2]};
+    std::string mas_2 = {ws[y][x + 2], ws[y - 1][x + 1], ws[y - 2][x]};
+    if (mas_1 == "MAS" && mas_2 == "MAS") {
+      count++;
+    }
+  }
+  // 4. Ms on LEFT, first M on top
+  if (y <= max_y - 4 && x <= max_x - 4) {
+    std::string mas_1 = {ws[y][x], ws[y + 1][x + 1], ws[y + 2][x + 2]};
+    std::string mas_2 = {ws[y + 2][x], ws[y + 1][x + 1], ws[y][x + 2]};
+    if (mas_1 == "MAS" && mas_2 == "MAS") {
+      count++;
+    }
+  }
+
+  return count;
+}
+
 int count_xmas_occurrences(std::vector<std::vector<char>> &ws, int x, int y) {
   int count = 0;
   int max_y = ws.size();
   int max_x = ws[0].size();
-  std::vector<char> match = {'M', 'A', 'S'};
   // check 8 possible directions clockwise
   // 1. vertical inverted xmas
   if (y >= 3) {
@@ -78,7 +118,8 @@ int main() {
   std::cin.tie(0);
 
   std::string line;
-  int count = 0;
+  int count_xmas = 0;
+  int count_x_mas = 0;
   std::vector<std::vector<char>> ws;
 
   while (std::cin) {
@@ -102,13 +143,24 @@ int main() {
       std::cout << " | " << c << "\n";
       if (c == 'X') {
         int found = count_xmas_occurrences(ws, x, y);
-        count += found;
-        std::cout << "found " << found << " occurrences of XMAS, " << count
-                  << "\n";
+        if (found >= 1) {
+          count_xmas += found;
+          std::cout << "found " << found << " occurrences of XMAS, "
+                    << count_xmas << "\n";
+        }
+      }
+
+      if (c == 'M') {
+        int found = count_x_mas_occurrences(ws, x, y);
+        if (found >= 1) {
+          count_x_mas += found;
+          std::cout << "found " << found << " occurrences of X-MAS, "
+                    << count_x_mas << "\n";
+        }
       }
     }
     std::cout << std::endl;
   }
 
-  std::cout << count;
+  std::cout << count_xmas << "," << count_x_mas;
 }
