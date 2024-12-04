b8e403c4bc28d7d213886a9e15c99a4aa48223d3
Gustavo Luchetti <gustavo@luchetti.dev>
2024-12-03 22:36:01 -0300

    wip: brute force

--- 2024/D02/main.cpp
+++ 2024/D02/main.cpp
@@ -1,6 +1,7 @@
 #include <cstdlib>
 #include <ios>
 #include <iostream>
+#include <pstl/glue_algorithm_defs.h>
 #include <sstream>
 #include <vector>
 
@@ -16,6 +17,28 @@ bool is_safe(int first, int second, bool is_asc) {
   return diff_ok;
 }
 
+bool is_report_safe(std::vector<int> report) {
+  bool is_basic_safe = true;
+
+  bool last_is_asc = (report[0] < report[1]);
+  int curr = report[0];
+  for (int i = 0; i < (report.size() - 1); i++) {
+    int next = report[i + 1];
+    bool is_asc = (curr < next);
+
+    bool safe = is_safe(curr, next, last_is_asc);
+    if (!safe) {
+      is_basic_safe = false;
+      break;
+    }
+
+    curr = next;
+    last_is_asc = is_asc;
+  }
+
+  return is_basic_safe;
+}
+
 int main() {
   std::ios::sync_with_stdio(0);
   std::cin.tie(0);
@@ -24,6 +47,7 @@ int main() {
   std::vector<int> report;
 
   int safe_levels = 0;
+  int adv_safe_levels = 0;
 
   while (std::cin) {
     getline(std::cin, line);
@@ -39,32 +63,37 @@ int main() {
       continue;
     }
 
-    bool is_basic_safe = true;
-    bool is_dampened_safe = true;
-    std::vector<int> bad_indexes = {};
+    bool is_basic_safe = is_report_safe(report);
+    bool is_adv_safe = true;
+    if (!is_basic_safe) {
+      is_adv_safe = false;
+      std::vector<int> temp = {};
+      for (int i = 0; i < report.size(); i++) {
+        temp = report;
+        temp.erase(temp.begin() + i);
 
-    bool last_is_asc = (report[0] < report[1]);
-    int last = report[0];
-    for (int i = 1; i < report.size(); i++) {
-      int curr = report[i];
-      bool is_asc = (last <= curr);
-
-      bool safe = is_safe(last, curr, last_is_asc);
-      if (!safe) {
-        is_basic_safe = false;
-        bad_indexes.push_back(i);
-        bad_indexes.push_back(i - 1);
+        if (is_report_safe(temp)) {
+          is_adv_safe = true;
+          std::cout << "\n";
+          for (int t : temp) {
+            std::cout << t;
+          }
+          std::cout << "\n";
+        }
       }
-
-      last = curr;
-      last_is_asc = is_asc;
     }
-    std::cout << line << " | " << is_basic_safe << "\n";
+
+    std::cout << line << " | " << is_basic_safe << ", " << is_adv_safe << "\n";
 
     if (is_basic_safe) {
       safe_levels++;
     }
+
+    if (is_adv_safe) {
+      adv_safe_levels++;
+    }
   }
 
-  std::cout << "basic safe: " << safe_levels << "\n";
+  std::cout << "\nbasic safe: " << safe_levels << "\n";
+  std::cout << "dampened safe: " << adv_safe_levels << "\n";
 }
