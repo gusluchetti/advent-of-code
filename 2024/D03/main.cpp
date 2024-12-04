8b79d1e61cab689c0f277d8cda38d8aaabfacc2e
Gustavo Luchetti <gustavo@luchetti.dev>
2024-12-03 23:37:55 -0300

    feat: solve d03 p2
    regex ftw!

--- 2024/D03/main.cpp
+++ 2024/D03/main.cpp
@@ -9,6 +9,8 @@ int main() {
 
   std::string line;
   int sum = 0;
+  int sum_toggled = 0;
+  bool on = true;
 
   while (std::cin) {
     getline(std::cin, line);
@@ -18,20 +20,38 @@ int main() {
     std::stringstream ss(line);
     std::cout << line << "\n";
 
-    std::regex e("mul\\((\\d{1,3}),(\\d{1,3})\\)");
-    std::regex_iterator<std::string::iterator> rit(line.begin(), line.end(), e);
+    // std::regex e("mul\\((\\d{1,3}),(\\d{1,3})\\)");
+    std::regex e2("(mul)\\((\\d{1,3}),(\\d{1,3})\\)|(do)\\(\\)|(don't)\\(\\)");
+
+    std::regex_iterator<std::string::iterator> rit(line.begin(), line.end(),
+                                                   e2);
     std::regex_iterator<std::string::iterator> rend;
 
     while (rit != rend) {
       auto match = *rit;
-      int first = std::stoi(match[1]);
-      int second = std::stoi(match[2]);
-      // std::cout << first << "*" << second << "\n";
-      sum += (first * second);
+
+      for (int i = 0; i < match.size(); i++) {
+        if (match[i] == "don't") {
+          on = false;
+        }
+        if (match[i] == "do") {
+          on = true;
+        }
+
+        if (match[i] == "mul") {
+          int first = std::stoi(match[i + 1]);
+          int second = std::stoi(match[i + 2]);
+          sum += (first * second);
+          if (on) {
+            sum_toggled += (first * second);
+          }
+        }
+      }
 
       rit++;
     }
 
-    std::cout << "\n" << sum << "\n";
+    std::cout << "\n" << sum;
+    std::cout << "\n" << sum_toggled << "\n";
   }
 }
