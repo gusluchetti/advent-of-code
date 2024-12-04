a7a8aaea4a4a46d1f6078297bff0db2f4aefa6c9
Gustavo Luchetti <gustavo@luchetti.dev>
2024-12-03 23:22:13 -0300

    feat: solve d3 p1
    sum was resetting on each line heheh

--- 2024/D03/main.cpp
+++ 2024/D03/main.cpp
@@ -1,16 +1,37 @@
 #include <iostream>
+#include <regex>
 #include <sstream>
+#include <string>
 
 int main() {
   std::ios::sync_with_stdio(0);
   std::cin.tie(0);
 
   std::string line;
+  int sum = 0;
 
   while (std::cin) {
     getline(std::cin, line);
+    if (line.empty()) {
+      break;
+    }
     std::stringstream ss(line);
-
     std::cout << line << "\n";
+
+    std::regex e("mul\\((\\d{1,3}),(\\d{1,3})\\)");
+    std::regex_iterator<std::string::iterator> rit(line.begin(), line.end(), e);
+    std::regex_iterator<std::string::iterator> rend;
+
+    while (rit != rend) {
+      auto match = *rit;
+      int first = std::stoi(match[1]);
+      int second = std::stoi(match[2]);
+      // std::cout << first << "*" << second << "\n";
+      sum += (first * second);
+
+      rit++;
+    }
+
+    std::cout << "\n" << sum << "\n";
   }
 }
