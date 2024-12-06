6213baeec6f8583b41292f9f0bcf8d369f66b259
Gustavo Luchetti <gustavo@luchetti.dev>
2024-12-06 16:03:16 -0300

    feat: print input

--- 2024/D06/main.cpp
+++ 2024/D06/main.cpp
@@ -1,12 +1,14 @@
 #include <cstdlib>
 #include <iostream>
 #include <string>
+#include <vector>
 
 int main() {
   std::ios::sync_with_stdio(0);
   std::cin.tie(0);
 
   std::string line;
+  std::vector<std::vector<char>> grid;
 
   while (std::cin) {
     getline(std::cin, line);
@@ -15,6 +17,13 @@ int main() {
       break;
     }
 
-    std::cout << line << "\n";
+    grid.push_back(std::vector<char>(line.begin(), line.end()));
+  }
+
+  for (size_t y = 0; y < grid.size() - 1; y++) {
+    for (size_t x = 0; x < grid[0].size() - 1; x++) {
+      std::cout << grid[y][x];
+    }
+    std::cout << "\n";
   }
 }
