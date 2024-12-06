79e462f1402bd553329aa54718c82614004fa267
Gustavo Luchetti <gustavo@luchetti.dev>
2024-12-06 15:53:19 -0300

    feat: setup d06

---
+++ 2024/D06/main.cpp
@@ -0,0 +1,20 @@
+#include <cstdlib>
+#include <iostream>
+#include <string>
+
+int main() {
+  std::ios::sync_with_stdio(0);
+  std::cin.tie(0);
+
+  std::string line;
+
+  while (std::cin) {
+    getline(std::cin, line);
+    if (std::cin.eof()) {
+      std::cout << "eof" << "\n";
+      break;
+    }
+
+    std::cout << line << "\n";
+  }
+}
