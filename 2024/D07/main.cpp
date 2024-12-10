17b6e3624a32c2f1f0c08db88ff88601fda317ac
Gustavo Luchetti <gustavo@luchetti.dev>
2024-12-10 09:57:35 -0300

    wip: testing subsets

--- 2024/D07/main.cpp
+++ 2024/D07/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <list>
 #include <sstream>
 #include <string>
 #include <vector>
@@ -31,10 +32,36 @@ int main() {
     equations.push_back(curr);
   }
 
+  std::list<char> opts = {'+', '*'};
+  int spaces = 0;
+  int sum = 0;
+  int initial_size = 0;
   for (auto e : equations) {
-    for (auto x : e) {
-      std::cout << x << " ";
+    int test_value = e[0];
+    bool finished = false;
+    std::vector<int> valid_subsets = {e[1]};
+    spaces = e.size() - 2;
+    while (valid_subsets.size() > 0) {
+      initial_size = valid_subsets.size();
+      for (int i = 0; i < valid_subsets.size(); i++) {
+        int v = valid_subsets[i];
+        for (int s = 2; s <= spaces + 1; s++) {
+          for (char o : opts) {
+            if (o == '+') {
+              v += e[s];
+            } else if (o == '*') {
+              v *= e[s];
+            }
+
+            if (v < test_value || (v == test_value && s == spaces + 1)) {
+              valid_subsets.push_back(v);
+            }
+          }
+        }
+      }
+      valid_subsets.erase(valid_subsets.begin(),
+                          valid_subsets.begin() + initial_size);
     }
-    std::cout << "\n";
   }
+  std::cout << "\n";
 }
