eee9833bacc2d2f3fb64db3e701f3291ea32313a
Gustavo Luchetti <gustavo@luchetti.dev>
2024-12-10 09:22:21 -0300

    feat: read input into list of ints

--- 2024/D07/main.cpp
+++ 2024/D07/main.cpp
@@ -1,20 +1,40 @@
 #include <iostream>
+#include <sstream>
+#include <string>
+#include <vector>
 
 int main() {
   std::ios::sync_with_stdio(0);
   std::cin.tie(0);
 
   std::string line;
+  std::vector<std::vector<int>> equations;
 
   // read grid
   while (std::cin) {
+    std::vector<int> curr;
     getline(std::cin, line);
     if (std::cin.eof()) {
       std::cout << "eof" << "\n";
       break;
     }
-    std::cout << line << "\n";
+
+    auto res = line.find(':');
+    int result = std::stoi(line.substr(0, res));
+    curr.push_back(result);
+    std::stringstream ss(line.substr(res + 2));
+    int num;
+    while (ss >> num) {
+      curr.push_back(num);
+    }
+
+    equations.push_back(curr);
   }
 
-  std::cout << "\nend";
+  for (auto e : equations) {
+    for (auto x : e) {
+      std::cout << x << " ";
+    }
+    std::cout << "\n";
+  }
 }
