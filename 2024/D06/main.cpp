52b78384429a6abbca5bb1efa63b175d0b7cb274
Gustavo Luchetti <gustavo@luchetti.dev>
2024-12-10 08:53:33 -0300

    feat: SOLVED IT finally!!!!

--- 2024/D06/main.cpp
+++ 2024/D06/main.cpp
@@ -13,8 +13,9 @@ bool add_obstacle = false;
 
 std::vector<std::vector<char>> g_grid;
 std::vector<std::pair<std::pair<int, int>, int>> g_path = {};
+std::set<std::pair<std::pair<int, int>, int>> loop_path = {};
 std::set<std::pair<int, int>> g_unique_locations = {};
-std::set<std::pair<int, int>> g_obstacles = {};
+int num_loops = 0;
 
 char guard_directions[4] = {'^', '>', 'v', '<'};
 std::pair<int, int> straight_next_pos[4] = {{-1, 0}, {0, +1}, {+1, 0}, {0, -1}};
@@ -44,18 +45,29 @@ void print_grid(Guard guard, std::vector<std::vector<char>> grid) {
   std::cout << "\n";
 }
 
-void p1(Guard guard, std::vector<std::vector<char>> grid) {
+void p1(Guard guard, std::vector<std::vector<char>> grid,
+        bool check_loop = false) {
+  loop_path = {};
   while (!done_p1) {
     g_path.push_back(std::make_pair(
         std::make_pair(guard.pos.first, guard.pos.second), guard.dir_index));
+    if (check_loop) {
+      auto res = loop_path.insert(std::make_pair(
+          std::make_pair(guard.pos.first, guard.pos.second), guard.dir_index));
+      if (res.second == false) {
+        num_loops++;
+        std::cout << "\nGOT LOOP! " << num_loops << "\n";
+        done_p1 = true;
+      }
+    }
     g_unique_locations.insert({guard.pos.first, guard.pos.second});
-    print_grid(guard, grid);
+    // print_grid(guard, grid);
     try {
       std::pair<int, int> next = {
           guard.pos.first + straight_next_pos[guard.dir_index].first,
           guard.pos.second + straight_next_pos[guard.dir_index].second};
       auto grid_char = grid.at(next.first).at(next.second);
-      while (grid_char == '#') {
+      while (grid_char == '#' || (grid_char == 'O' && check_loop)) {
         guard.rotate();
         next = {guard.pos.first + straight_next_pos[guard.dir_index].first,
                 guard.pos.second + straight_next_pos[guard.dir_index].second};
@@ -70,6 +82,36 @@ void p1(Guard guard, std::vector<std::vector<char>> grid) {
       done_p1 = true;
       continue;
     }
+  }
+}
+
+void p2(Guard guard, std::vector<std::vector<char>> grid) {
+  auto init_pos = g_path[0];
+  std::vector<std::pair<int, int>> obstacles = {};
+  for (auto it = g_unique_locations.begin(); it != g_unique_locations.end();
+       it++) {
+    auto coord = *it;
+    if (coord.first == init_pos.first.first &&
+        coord.second == init_pos.first.second) {
+      // do nothing
+    } else {
+      obstacles.push_back(coord);
+    }
+  }
+
+  done_p1 = false;
+  while (!done_p2) {
+    for (size_t i = 0; i < obstacles.size(); i++) {
+      auto obstacle = obstacles[i];
+      std::cout << obstacle.first << "," << obstacle.second << " ";
+
+      grid[obstacle.first][obstacle.second] = 'O';
+      std::cout << "\nNEW TEST!\n";
+      p1(guard, grid, true);
+      done_p1 = false;
+      grid[obstacle.first][obstacle.second] = '.';
+    }
+    done_p2 = true;
   }
 }
 
@@ -104,9 +146,9 @@ int main() {
   }
 
   p1(guard, g_grid);
-
-  std::cout << "\n\n";
-  std::cout << "path: " << g_path.size() << " \n";
+  std::cout << "\npath: " << g_path.size() << " \n";
   std::cout << "unique locations: " << g_unique_locations.size() << " \n";
-  std::cout << "num loops: " << g_obstacles.size() << " \n";
+
+  p2(guard, g_grid);
+  std::cout << "\nnum loops: " << num_loops << " \n";
 }
