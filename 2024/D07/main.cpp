ed098dde98281c86305e9d4b4412df3e2ee6e94f
Gustavo Luchetti <gustavo@luchetti.dev>
2024-12-10 08:56:24 -0300

    feat: prep d07

---
+++ 2024/D07/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+int main() {
+  std::ios::sync_with_stdio(0);
+  std::cin.tie(0);
+
+  std::string line;
+
+  // read grid
+  while (std::cin) {
+    getline(std::cin, line);
+    if (std::cin.eof()) {
+      std::cout << "eof" << "\n";
+      break;
+    }
+    std::cout << line << "\n";
+  }
+
+  std::cout << "\nend";
+}
