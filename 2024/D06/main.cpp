3adc7ea464c3f22c6a2b548bc25296d860c55522
Gustavo Luchetti <gustavo@luchetti.dev>
2024-12-10 08:07:16 -0300

    feat: cleanup code

--- 2024/D06/main.cpp
+++ 2024/D06/main.cpp
@@ -7,19 +7,15 @@
 #include <utility>
 #include <vector>
 
-// NOTE:
-// possible obstacle for infinite loop HAS to be on the path
-// obstacle placement (on path) leads to infinite line to the right
-// if line reaches path + another position (to check direction) = loop
-
 std::vector<std::vector<char>> grid;
 char guard_directions[4] = {'^', '>', 'v', '<'};
 std::pair<int, int> straight_next_pos[4] = {{-1, 0}, {0, +1}, {+1, 0}, {0, -1}};
 std::pair<int, int> right_next_pos[4] = {{0, +1}, {+1, 0}, {0, -1}, {-1, 0}};
 
-bool done = false;
+bool done_p1 = false;
+bool done_p2 = false;
 bool add_obstacle = false;
-std::vector<std::pair<int, int>> path = {};
+std::vector<std::pair<std::pair<int, int>, int>> path = {};
 std::set<std::pair<int, int>> unique_locations = {};
 std::set<std::pair<int, int>> obstacles = {};
 
@@ -35,110 +31,6 @@ struct Guard {
     }
   }
 };
-
-// 1896 too low | 1922, 2013 incorrect
-bool check_path_collision(Guard guard_cp,
-                          std::vector<std::vector<char>> grid_cp) {
-  bool should_quit = false;
-
-  guard_cp.rotate();
-  std::vector<std::pair<int, int>> curr_path = {
-      {guard_cp.pos.first + straight_next_pos[guard_cp.dir_index].first,
-       guard_cp.pos.second + straight_next_pos[guard_cp.dir_index].second}};
-
-  while (!should_quit) {
-    int dupe_count = 0;
-
-    std::pair<int, int> next = {
-        curr_path.back().first + straight_next_pos[guard_cp.dir_index].first,
-        curr_path.back().second + straight_next_pos[guard_cp.dir_index].second};
-
-    try {
-      auto on_grid = grid_cp.at(next.first).at(next.second);
-      if (on_grid == '#') {
-        guard_cp.rotate();
-        next = {
-            curr_path.back().first +
-                straight_next_pos[guard_cp.dir_index].first,
-            curr_path.back().second +
-                straight_next_pos[guard_cp.dir_index].second,
-        };
-      }
-    } catch (const std::out_of_range &e) {
-      should_quit = true;
-      continue;
-    }
-
-    curr_path.push_back(next);
-
-    auto penultimate = curr_path[curr_path.size() - 2];
-    auto last = curr_path[curr_path.size() - 1];
-    for (size_t j = 0; j < curr_path.size() - 1; j++) {
-      if (curr_path[j] == penultimate && curr_path[j + 1] == last) {
-        dupe_count++;
-        if (dupe_count >= 2) {
-          return true;
-        }
-      }
-    }
-  }
-
-  return false;
-}
-
-bool move_guard(Guard &guard) {
-  path.push_back(guard.pos);
-  if (add_obstacle) {
-    std::cout << "LOOPED at " << guard.pos.first << "," << guard.pos.second
-              << "\n";
-    obstacles.insert(guard.pos);
-    add_obstacle = false;
-  }
-  // if (path.size() >= 27) {
-  //   return true;
-  // }
-  auto res = unique_locations.insert(guard.pos);
-  std::pair<int, int> next = {
-      guard.pos.first + straight_next_pos[guard.dir_index].first,
-      guard.pos.second + straight_next_pos[guard.dir_index].second};
-
-  std::cout << path.size() << " | try " << guard.pos.first << ","
-            << guard.pos.second;
-  std::cout << " to " << next.first << "," << next.second;
-
-  char grid_char = '.';
-  try {
-    grid_char = grid.at(next.first).at(next.second);
-  } catch (const std::out_of_range &e) {
-    return true;
-  }
-
-  grid[guard.pos.first][guard.pos.second] = '.';
-
-  while (grid_char == '#') {
-    guard.rotate();
-    next = {guard.pos.first + straight_next_pos[guard.dir_index].first,
-            guard.pos.second + straight_next_pos[guard.dir_index].second};
-    try {
-      grid_char = grid.at(next.first).at(next.second);
-    } catch (const std::out_of_range &e) {
-      return true;
-    }
-  }
-
-  auto cp_grid = grid;
-  cp_grid[next.first][next.second] = '#';
-  bool is_loop = check_path_collision(guard, cp_grid);
-  if (is_loop) {
-    add_obstacle = true;
-  }
-  std::cout << "\n";
-  guard.pos = next;
-
-  grid[guard.pos.first][guard.pos.second] = guard.dir();
-
-  return false;
-}
 
 void print_grid(Guard &guard) {
   for (size_t y = 0; y < grid.size(); y++) {
@@ -160,6 +52,8 @@ int main() {
   Guard guard;
 
   int count = 0;
+
+  // read grid
   while (std::cin) {
     getline(std::cin, line);
     if (std::cin.eof()) {
@@ -180,22 +74,35 @@ int main() {
     count++;
   }
 
-  while (!done) {
-    // print_grid(guard);
-    done = move_guard(guard);
+  while (!done_p1) {
+    path.push_back(std::make_pair(
+        std::make_pair(guard.pos.first, guard.pos.second), guard.dir_index));
+    unique_locations.insert({guard.pos.first, guard.pos.second});
+    print_grid(guard);
+    try {
+      std::pair<int, int> next = {
+          guard.pos.first + straight_next_pos[guard.dir_index].first,
+          guard.pos.second + straight_next_pos[guard.dir_index].second};
+      auto grid_char = grid.at(next.first).at(next.second);
+      while (grid_char == '#') {
+        guard.rotate();
+        next = {guard.pos.first + straight_next_pos[guard.dir_index].first,
+                guard.pos.second + straight_next_pos[guard.dir_index].second};
+        grid_char = grid.at(next.first).at(next.second);
+      }
+
+      grid[guard.pos.first][guard.pos.second] = '.';
+      guard.pos = next;
+      grid[next.first][next.second] = guard.dir();
+
+    } catch (const std::out_of_range &e) {
+      done_p1 = true;
+      continue;
+    }
   }
 
   std::cout << "\n\n";
   std::cout << "path: " << path.size() << " \n";
-  // for (auto p : path) {
-  //   std::cout << "(" << p.first << "," << p.second << ")";
-  // }
   std::cout << "unique locations: " << unique_locations.size() << " \n";
-  // for (auto u : unique_locations) {
-  //   std::cout << "(" << s.first << "," << s.second << ")";
-  // }
   std::cout << "num loops: " << obstacles.size() << " \n";
-  // for (auto o : obstacles) {
-  //   std::cout << "(" << o.first << "," << o.second << ")";
-  // }
 }
