66558f50bcb76a2eaec71d8d9eac4cafe233ca2f
Gustavo Luchetti <gustavo@luchetti.dev>
2024-12-11 11:41:30 -0300

    wip: populate disk map

--- 2024/D09/main.cpp
+++ 2024/D09/main.cpp
@@ -1,10 +1,12 @@
 #include <iostream>
 #include <string>
+#include <vector>
 
 int main() {
   std::ios::sync_with_stdio(0);
   std::cin.tie(0);
 
+  std::vector<int> values;
   int disk_size = 0;
 
   // read grid
@@ -20,11 +22,24 @@ int main() {
       int num = c - '0';
       std::cout << num << "\n";
       disk_size += num;
+      values.push_back(num);
     }
   }
 
-  int temp[disk_size];
-  for (size_t i = 0; i < sizeof(temp); i++) {
-    int x = temp[i];
+  int diskmap[disk_size];
+  for (size_t i = 0; i < values.size(); i++) {
+    int num = values[i];
+    for (int x = 0; x < num; x++) {
+      if (i % 2 == 0 || i == 0) {
+        diskmap[i + x] = i;
+      } else {
+      }
+    }
   }
+
+  for (size_t i = 0; i < sizeof(diskmap); i++) {
+    int x = diskmap[i];
+    std::cout << x;
+  }
+  std::cout << "\n";
 }
