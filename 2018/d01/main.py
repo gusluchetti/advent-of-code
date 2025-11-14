a95a76010bad7daf35b76ac1a86203611aa9e45c
Gustavo Luchetti <gustavo@luchetti.dev>
2025-11-14 10:20:47 -0300

    feat: d01 part 1

--- 2018/d01/main.py
+++ 2018/d01/main.py
@@ -1,1 +1,10 @@
-print("Hello World!")
+from pathlib import Path
+script_dir = Path(__file__).parent
+file_path = script_dir / './input.txt'
+
+freq = 0
+lines = file_path.read_text().splitlines()
+
+freq = sum(float(line) for line in lines)
+
+print(freq)
